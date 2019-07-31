35abcfc2a3d136e87d562799012997dfd47f7032
dennyangesti <dny_angesti@yahoo.com>
2019-07-31 10:44:04 +0700

    add get /

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get('/', (req, res) => {
+    res.send(`<h1>Welcome to Heroku App</h1>`)
+})
+
 app.listen(port, () => {
     console.log('Berhasil Running di ' + port);
 
